fd939f49b290ff0b48f801c062cd3707643a5ed4
GGiry <geoffrey.giry@spirtech.com>
2022-07-06 13:45:09 +0200

    Add Vec3 - operator.

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,6 +1,6 @@
 use derivative::Derivative;
 use std::fmt::{Display, Formatter};
-use std::ops::{Add, AddAssign, DivAssign, Index, MulAssign, Neg};
+use std::ops::{Add, AddAssign, DivAssign, Index, MulAssign, Neg, Sub};
 
 #[derive(Copy, Clone, Derivative)]
 #[derivative(Default)]
@@ -90,6 +90,18 @@ impl Add for Vec3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        };
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        return Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
         };
     }
 }
@@ -306,5 +318,33 @@ mod tests {
         assert_eq!(5.0, vec_c.x());
         assert_eq!(7.0, vec_c.y());
         assert_eq!(9.0, vec_c.z());
+    }
+
+    #[test]
+    fn vec3_sub() {
+        let vec_a = Vec3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let vec_b = Vec3 {
+            x: 4.0,
+            y: 5.0,
+            z: 6.0,
+        };
+
+        let vec_c = vec_a - vec_b;
+
+        assert_eq!(1.0, vec_a.x());
+        assert_eq!(2.0, vec_a.y());
+        assert_eq!(3.0, vec_a.z());
+
+        assert_eq!(4.0, vec_b.x());
+        assert_eq!(5.0, vec_b.y());
+        assert_eq!(6.0, vec_b.z());
+
+        assert_eq!(-3.0, vec_c.x());
+        assert_eq!(-3.0, vec_c.y());
+        assert_eq!(-3.0, vec_c.z());
     }
 }
