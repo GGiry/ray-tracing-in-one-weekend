b6e96d5a52e73c41ca44f96b4d2fdbb8acc81205
Giry Geoffrey <giry.geoffrey@gmail.com>
2022-07-10 14:37:25 +0200

    USe hittable list in main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::Write;
 
 mod hittable;
+mod hittable_list;
 mod ray;
 mod sphere;
 mod utils;
@@ -10,7 +11,10 @@ mod vec3;
 use crate::utils::write_color;
 use crate::vec3::{dot, Vec3};
 
+use crate::hittable::Hittable;
+use crate::hittable_list::HittableList;
 use crate::ray::Ray;
+use crate::sphere::Sphere;
 use Vec3 as Color;
 use Vec3 as Point3;
 
@@ -18,29 +22,15 @@ fn linear_blend(t: f64, start: Color, end: Color) -> Color {
     return (1.0 - t) * start + t * end;
 }
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
-    let oc = ray.origin() - center;
-    let a = ray.direction().length_squared();
-    let half_b = dot(&oc, &ray.direction());
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-
-    if discriminant < 0.0 {
-        return -1.0;
-    }
-
-    return (-half_b - discriminant.sqrt()) / a;
-}
-
-fn ray_color(ray: &Ray) -> Color {
-    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
-    if t > 0.0 {
-        let normal = (ray.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
-        return 0.5 * Color::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
-    }
-    let unit_direction = ray.direction().unit_vector();
+fn ray_color(ray: &Ray, world: &HittableList) -> Color {
     let white = Color::new(1.0, 1.0, 1.0);
     let light_blue = Color::new(0.5, 0.7, 1.0);
+
+    if let Some(hit) = world.hit(ray, 0.0, f64::INFINITY) {
+        return 0.5 * (hit.normal + white);
+    }
+
+    let unit_direction = ray.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
 
     return linear_blend(t, white, light_blue);
@@ -54,6 +44,11 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width: u32 = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
+
+    // World
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
     let viewport_height = 2.0;
@@ -82,7 +77,7 @@ fn main() {
                 origin,
                 lower_left_corner + u * horizontal + v * vertical - origin,
             );
-            let pixel_color = ray_color(&ray);
+            let pixel_color = ray_color(&ray, &world);
 
             write_color(&mut file, pixel_color);
         }
@@ -107,21 +102,5 @@ mod tests {
         assert_eq!(white, grey_0_0);
         assert_eq!(expected_grey, grey_0_5);
         assert_eq!(black, grey_1_0);
-    }
-
-    #[test]
-    fn hit_sphere_test() {
-        let center = Point3::new(0.0, 0.0, 0.0);
-        let radius = 1.0;
-
-        let origin = Point3::new(0.0, 0.0, 3.0);
-        let direction = Vec3::new(0.0, 0.0, -1.0);
-        let ray_touching = Ray::new(origin, direction);
-
-        let direction2 = Vec3::new(0.0, 1.0, 0.0);
-        let ray_not_touching = Ray::new(origin, direction2);
-
-        assert!(0.0 < hit_sphere(center, radius, &ray_touching));
-        assert!(0.0 > hit_sphere(center, radius, &ray_not_touching));
     }
 }
