fd2ea908068653d7bff4630ec9127fdc86e7dea2
GGiry <geoffrey.giry@spirtech.com>
2022-07-13 15:18:25 +0200

    Use iter instead of "for in" for rows computation.

--- src/main.rs
+++ src/main.rs
@@ -74,26 +74,30 @@ fn main() {
         .expect("Unable to write data");
     file.write_all(b"255\n").expect("Unable to write data");
 
-    let mut image = Vec::new();
+    let mut image: Vec<u8> = Vec::new();
 
     for index_height in (0..image_height).rev() {
         eprintln!("Scanlines remaining: {index_height}");
-        for index_width in 0..image_width {
-            let mut pixel_color: Vec3 = (0..samples_per_pixel)
-                .map(|_| {
-                    let u = (index_width as f64 + random_f64()) / ((image_width - 1) as f64);
-                    let v = (index_height as f64 + random_f64()) / ((image_height - 1) as f64);
-                    let ray = camera.get_ray(u, v);
-                    return ray_color(&ray, &world, max_depth);
-                })
-                .sum();
+        let mut rows = (0..image_width)
+            .flat_map(|index_width| {
+                let mut pixel_color: Vec3 = (0..samples_per_pixel)
+                    .map(|_| {
+                        let u = (index_width as f64 + random_f64()) / ((image_width - 1) as f64);
+                        let v = (index_height as f64 + random_f64()) / ((image_height - 1) as f64);
+                        let ray = camera.get_ray(u, v);
+                        return ray_color(&ray, &world, max_depth);
+                    })
+                    .sum();
 
-            image.push(color_to_rbg(pixel_color, samples_per_pixel));
-        }
+                return color_to_rbg(pixel_color, samples_per_pixel);
+            })
+            .collect::<Vec<u8>>();
+
+        image.append(&mut rows);
     }
 
-    for rbg in image {
-        file.write_all(format!("{} {} {}\n", rbg[0], rbg[1], rbg[2]).as_bytes())
+    for rgb in image.chunks(3) {
+        file.write_all(format!("{} {} {}\n", rgb[0], rgb[1], rgb[2]).as_bytes())
             .expect("Unable to write data");
     }
 }
